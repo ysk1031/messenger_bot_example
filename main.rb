0c1473211a69480474ddf5556bd535a4a6d066a9
ysk1031 <yusukeaono.1031@gmail.com>
2016-04-17 22:21:38 +0900

    Add parrotry response

--- main.rb
+++ main.rb
@@ -1,5 +1,9 @@
 require "sinatra"
 require "sinatra/reloader" if development?
+require "json"
+require 'rest-client'
+require "dotenv"
+Dotenv.load
 
 get "/" do
   "Hello, world!"
@@ -10,4 +14,36 @@ get "/callback" do
     return "Error, wrong validation token"
   end
   params["hub.challenge"]
+end
+
+post "/callback" do
+  request_body = JSON.parse(request.body.read)
+  messaging_events = request_body["entry"][0]["messaging"]
+  messaging_events.each do |event|
+    if !event["message"].nil? && !event["message"]["text"].nil?
+      sender = event["sender"]["id"]
+      text = event["message"]["text"]
+      bot_response(sender, text)
+    end
+  end
+
+  status 201
+  body ''
+end
+
+
+def bot_response(sender, text)
+  request_endpoint = "https://graph.facebook.com/v2.6/me/messages?access_token=#{ENV["FACEBOOK_PAGE_TOKEN"]}"
+  request_body = {
+    recipient: {
+      id: sender
+    },
+    message: {
+      text: text
+    }
+  }.to_json
+
+  RestClient.post(request_endpoint, request_body, content_type: :json, accespt: :json) do |response, request, result, &block|
+    p response.body
+  end
 end
